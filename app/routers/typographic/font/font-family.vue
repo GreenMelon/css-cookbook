fcfb7a79d897f9436684b652532af70d4e79b53b
caigua <caigua@huanleguang.com>
2016-12-20 11:26:07 +0800

    Typographic: font - fontFamily

---
+++ app/routers/typographic/font/font-family.vue
@@ -0,0 +1,30 @@
+<style lang="less" scoped>
+    p {
+        margin: 10px 0;
+    }
+    .MSYH {
+        font-family: Arial, "Helvetica Neue", "Microsoft YaHei";
+    }
+</style>
+
+<template>
+    <main>
+        <nav>xxxx</nav>
+        <ul>
+            <li>
+                <p class="MSYH">微软雅黑 Light</p>
+                <pre class="pre-code">
+                    <code>
+                        .MSYH {
+                            font-family: Arial, "Helvetica Neue", "Microsoft YaHei";
+                        }
+                    </code>
+                </pre>
+            </li>
+        </ul>
+    </main>
+</template>
+
+<script>
+    module.exports = {}
+</script>
---
+++ app/routers/typographic/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <main>
+        <nav>xxxx</nav>
+        <ul class="list-category mod-categories">
+            <li v-for="category in categories" class="item-main">
+                <h2 v-text="category.name"></h2>
+                <ul class="list-instance">
+                    <li v-for="instance in category.instances" class="item-sub">
+                        <router-link :to="instance.route">{{ instance.name }}
+                        </router-link>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </main>
+</template>
+
+<script>
+    module.exports = {
+        data() {
+            return {
+                categories: [
+                    {
+                        name: 'font',
+                        instances: [
+                            {
+                                name: 'font-family',
+                                route: '/typographic/font/font-family'
+                            }
+                        ]
+                    }
+                ]
+            }
+        },
+        methods: {
+            //
+        },
+        created() {
+            //
+        }
+    }
+</script>
