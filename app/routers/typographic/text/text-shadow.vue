66b1202c9db16c0abffb525bfbe0dff86ad9a8da
caigua <caigua@huanleguang.com>
2017-02-16 23:50:25 +0800

    Typographic text-shadow

---
+++ app/routers/typographic/text/text-shadow.vue
@@ -0,0 +1,21 @@
+<style lang="less" scoped>
+    p {
+        font-size: 24px;
+        text-shadow: 10px 10px 2px #ccc;
+    }
+</style>
+
+<template>
+    <main>
+        <nav>xxxx</nav>
+        <ul>
+            <li>
+                <p>text-shadow</p>
+            </li>
+        </ul>
+    </main>
+</template>
+
+<script>
+    module.exports = {}
+</script>
