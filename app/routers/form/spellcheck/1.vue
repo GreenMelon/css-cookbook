81c8231aeb4ac5d84cb96397505abfd5052d4496
caigua <caigua@huanleguang.com>
2017-02-21 21:04:33 +0800

    spellcheck

---
+++ app/routers/form/spellcheck/1.vue
@@ -0,0 +1,67 @@
+<style lang="less" scoped>
+    input {
+        padding: 10px;
+        outline: none;
+    }
+    .not-spellcheck {
+        &::-moz-selection {
+            // color: #fff;
+            // background-color: #007dd4;
+        }
+        &::selection {
+            // color: #fff;
+            // background-color: #00f;
+        }
+    }
+</style>
+
+<template>
+    <main>
+        <nav>xxxx</nav>
+        <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/spellcheck">spellcheck</a>
+        <table>
+            <tr>
+                <td>
+                    <p>
+                        <input
+                            v-model="a"
+                            @focus="select($event)"
+                            type="text"
+                            spellcheck="true"
+                        >
+                    </p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p>
+                        <input
+                            v-model="b"
+                            @focus="select($event)"
+                            class="not-spellcheck"
+                            type="text"
+                            spellcheck="false"
+                        >
+                    </p>
+                </td>
+            </tr>
+        </table>
+    </main>
+</template>
+
+<script>
+    import $ from 'jquery';
+    module.exports = {
+        data() {
+            return {
+                a: 'xxxxx',
+                b: 'yyyyy'
+            }
+        },
+        methods: {
+            select(e) {
+                $(e.target).select();
+            }
+        }
+    };
+</script>
