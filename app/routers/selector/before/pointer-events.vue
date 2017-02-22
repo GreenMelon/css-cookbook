6dea8b2baca84e212febbdd84bb81d739c72d6b3
caigua <caigua@huanleguang.com>
2017-02-23 05:18:23 +0800

    points-event

---
+++ app/routers/selector/before/pointer-events.vue
@@ -0,0 +1,50 @@
+<style lang="less" scoped>
+    li {
+        text-align: center;
+    }
+    button {
+        position: relative;
+        display: inline-block;
+        min-width: 60px;
+        padding: 0 10px;
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+        border: none;
+        outline: none;
+        color: #fff;
+        background-color: #007dd4;
+        cursor: pointer;
+        &::after {
+            content: "";
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            width: 120px;
+            height: 60px;
+            transform: translate(-50%, -50%);
+            // pointer-events: none;
+        }
+    }
+</style>
+
+<template>
+    <main>
+        <nav>xxxx</nav>
+        <ul>
+            <li>
+                <button @click="submit($event)" type="button">submit</button>
+            </li>
+        </ul>
+    </main>
+</template>
+
+<script>
+    module.exports = {
+        methods: {
+            submit(e) {
+                console.log(e.target);
+            }
+        }
+    };
+</script>
