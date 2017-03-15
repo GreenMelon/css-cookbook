4b84679325050bc4e6b9c34dda1f8609dac96439
caigua <caigua@huanleguang.com>
2017-03-15 21:57:05 +0800

    Typographic - writing-mode

---
+++ app/page/typographic/text/writing-mode.vue
@@ -0,0 +1,38 @@
+<style lang="less" scoped>
+        p.horizontal-tb {
+            height: 50px;
+            writing-mode: horizontal-tb;
+        }
+        p.vertical-lr {
+            writing-mode: vertical-lr;
+        }
+        p.vertical-rl {
+            writing-mode: vertical-rl;
+        }
+</style>
+
+<template>
+    <main>
+        <nav>xxxx</nav>
+        <button @click="changeMode('horizontal-tb')" type="button">horizontal-tb</button>
+        <button @click="changeMode('vertical-lr')" type="button">vertical-lr</button>
+        <button @click="changeMode('vertical-rl')" type="button">vertical-rl</button>
+        <p :style="{'writing-mode': mode}">writing-mode 改变文字的书写模式</p>
+    </main>
+</template>
+
+<script>
+    module.exports = {
+        data() {
+            return {
+                mode: ''
+            }
+        },
+        methods: {
+            changeMode(mode) {
+                this.mode = mode;
+            }
+        },
+        created() {}
+    };
+</script>
