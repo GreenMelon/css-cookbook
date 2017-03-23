488c4faa669669007694dcf782597cd22f309dbb
caigua <caigua@huanleguang.com>
2017-03-23 10:20:04 +0800

    File API

--- app/docs/HTMLImageElement.md
+++ app/docs/HTMLImageElement.md
@@ -5,3 +5,10 @@ https://developer.mozilla.org/zh-CN/docs/Web/HTML/Element/img
 https://www.sitepoint.com/how-to-build-responsive-images-with-srcset/
 
 http://blog.zhusee.in/post/248199/basic-responsive-image-with-srcset-property
+
+http://developer.51cto.com/art/201309/410720.htm
+
+http://nicolasgallagher.com/responsive-images-using-css3/
+
+https://www.w3cplus.com/css3/srcset-sizes.html
+
--- app/page/image/sizes/001.vue
+++ app/page/image/sizes/001.vue
@@ -5,7 +5,7 @@
     <main>
         <img
             src="/app/page/image/img/1.png"
-            srcset="/app/page/image/img/1.png 56w, /app/page/image/img/2.png 112w, /app/page/image/img/4.png, 224w"
+            srcset="/app/page/image/img/1.png 56w, /app/page/image/img/2.png 112w, /app/page/image/img/4.png 224w"
             sizes="(max-width: 768px) 56px, 112px"
         >
     </main>
