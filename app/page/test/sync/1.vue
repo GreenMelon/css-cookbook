4cdf340ee4f4c7940707603d8c7233544969769b
caigua <caigua@huanleguang.com>
2017-05-22 20:20:27 +0800

    VUE2.x sync写法

---
+++ app/page/test/sync/1.vue
@@ -0,0 +1,26 @@
+<style lang="less" scoped>
+</style>
+
+<template>
+    <main>
+        <Child :x="num" @update="(val) => { num = val}"></Child>
+    </main>
+</template>
+
+<script>
+    import './child';
+
+    export default {
+        data() {
+            return {
+                num: 1
+            }
+        },
+        methods: {
+            //
+        },
+        mounted() {
+            //
+        }
+    };
+</script>
