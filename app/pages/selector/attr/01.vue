f6c7028b5668e9c80d24ef12e2f7a3e823ac26ee
caigua <caigua@huanleguang.com>
2018-03-09 11:45:43 +0800

    Selector - [attribute]

---
+++ app/pages/selector/attr/01.vue
@@ -0,0 +1,67 @@
+<style lang="less" scoped>
+    p {
+        font-size: 20px;
+    }
+    [class=val] {
+        color: orange;
+    }
+    [class|=val] {
+        color: purple;
+    }
+    [class~=val] {
+        color: deeppink;
+    }
+    [class*=val] {
+        color: gold;
+    }
+    [class^=val] {
+        color: aquamarine;
+    }
+    [class$=val] {
+        color: pink;
+    }
+</style>
+
+<template>
+    <main>
+        <h1>属性选择器</h1>
+
+        <pre>
+            <code>
+                [attr=val]：属性值为val;
+                [attr|=val]：属性值为val或者以val-开头;
+                [attr~=val]：属性值为val或者以多个空格分开含有val;
+                [attr*=val]：属性值包含val;
+                [attr^=val]：属性值以val开头;
+                [attr$=val]：属性值以val结尾.
+            </code>
+        </pre>
+
+        <p class="val">class="val"</p>
+
+        <p class="val">class="val"</p>
+        <p class="val-">class="val-"</p>
+        <p class="val-a">class="val-a"</p>
+
+        <p class="val">class="val"</p>
+        <p class="val a">class="val a"</p>
+        <p class="val ">class="val "</p>
+        <p class="a val">class="a val"</p>
+
+        <p class="val">class="val"</p>
+        <p class="value">class="value"</p>
+        <p class="a-val-b">class="a-val-b"</p>
+        <p class="a-val">class="a-val"</p>
+
+        <p class="val">class="val"</p>
+        <p class="value">class="value"</p>
+
+        <p class="val">class="val"</p>
+        <p class="a val">class="a val"</p>
+        <p class="a-val">class="a-val"</p>
+    </main>
+</template>
+
+<script>
+    export default {};
+</script>
