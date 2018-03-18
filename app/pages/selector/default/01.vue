aac08433953a6da3d08b76ca97bf0afdb4affa7d
caigua <caigua@huanleguang.com>
2018-03-18 23:11:15 +0800

    Selector - :default

--- app/pages/selector/default/01.vue
+++ app/pages/selector/default/01.vue
@@ -3,6 +3,9 @@
         font-size: 16px;
         background-color: red;
     }
+    input:default + label::after {
+        content: "(推荐)";
+    }
 </style>
 
 <template>
@@ -13,6 +16,20 @@
                 <option value="2" selected>2</option>
                 <option value="3">3</option>
             </select>
+            <div>
+                <p>
+                    <input type="radio" name="option" id="radio-1">
+                    <label for="radio-1">选项1</label>
+                </p>
+                <p>
+                    <input type="radio" name="option" id="radio-2" checked>
+                    <label for="radio-2">选项2</label>
+                </p>
+                <p>
+                    <input type="radio" name="option" id="radio-3">
+                    <label for="radio-3">选项3</label>
+                </p>
+            </div>
         </form>
     </main>
 </template>
