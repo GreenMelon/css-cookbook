13b40bcbf360779407b1cf7aa9c2900990f15f30
caigua <caigua@huanleguang.com>
2018-10-24 15:42:03 +0800

    Selector - indeterminate

---
+++ app/pages/selector/indeterminate/01.vue
@@ -0,0 +1,55 @@
+<style lang="less" scoped>
+    input:indeterminate + label {
+        color: red;
+    }
+
+    progress:indeterminate {
+        border: 1px solid orange;
+    }
+</style>
+
+<template>
+    <main>
+        <div>
+            <h1>复选框</h1>
+            <p>
+                <input id="checkboxId" type="checkbox" indeterminate="true">
+                <label for="checkboxId">Checkbox</label>
+            </p>
+        </div>
+
+        <div>
+            <h1>进度条</h1>
+            <p>
+                <progress /><br>
+                <progress value="15"/><br>
+                <progress value="10" max="20"/>
+            </p>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        methods: {
+            init() {
+                const input = document.querySelector('input');
+
+                console.log({
+                    indeterminate: input.indeterminate,
+                    checked: input.checked,
+                });
+
+                input.indeterminate = true;
+
+                console.log({
+                    indeterminate: input.indeterminate,
+                    checked: input.checked,
+                });
+            },
+        },
+        mounted() {
+            this.init();
+        },
+    };
+</script>
