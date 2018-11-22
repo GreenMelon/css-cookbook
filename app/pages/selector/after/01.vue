28a0b31d7315db71f9cdd1407306750860754c27
caigua <caigua@huanleguang.com>
2018-11-22 23:47:22 +0800

    Selector - before

---
+++ app/pages/selector/after/01.vue
@@ -0,0 +1,17 @@
+<style lang="less" scoped>
+    #x::after{
+        content: "\A";
+        white-space: pre;
+    }
+</style>
+
+<template>
+    <main>
+        <span id="x">x</span>
+        <span>y</span>
+    </main>
+</template>
+
+<script>
+    export default {};
+</script>
