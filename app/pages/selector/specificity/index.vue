9acea517f691eb0f988a4fb14fe93463b93c194b
caigua <caigua@huanleguang.com>
2019-06-27 22:52:50 +0800

    selector: specificity

--- app/pages/selector/specificity/index.vue
+++ app/pages/selector/specificity/index.vue
@@ -1,9 +1,22 @@
 <style lang="less">
+.green {
+    color: green;
+}
+.red {
+    color: red;
+}
+.green.red {
+    color: yellow;
+}
+.red.green {
+    color: orange;
+}
 </style>
 
 <template>
     <main>
-        //
+        <h2 class="green red">caigua</h2>
+        <h2 class="red green">caigua</h2>
     </main>
 </template>
 
