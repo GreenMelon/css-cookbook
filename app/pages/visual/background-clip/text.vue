914cd1534305486f9b8a8d3b0d948cd51a5fe339
caigua <caigua@huanleguang.com>
2019-08-19 10:37:44 +0800

    visual: background-clip: text

--- app/pages/visual/background-clip/text.vue
+++ app/pages/visual/background-clip/text.vue
@@ -1,20 +1,38 @@
 <style lang="less" scoped>
-    p {
-        font-size: 80px;
-        text-align: center;
-        text-transform: uppercase;
-        color: transparent;
-        background-image: url(~images/blue-sky.jpg);
-        background-repeat: no-repeat;
-        background-position: 50% 78%;
-        -webkit-background-clip: text;
-        background-clip: text;
-    }
+main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: sans-serif;
+    font-size: 100px;
+    font-weight: bold;
+    text-transform: uppercase;
+    background: black;
+}
+
+.p1 {
+    color: transparent;
+    background-image: url(~images/blue-sky.jpg);
+    background-repeat: no-repeat;
+    background-position: 50% 78%;
+    -webkit-background-clip: text;
+    background-clip: text;
+}
+.p2 {
+    color: transparent;
+    background-image: url(https://media.giphy.com/media/YnkuiP6WFNM2ezj0XV/giphy.gif);
+    background-size: contain;
+    background-position: top left;
+    -webkit-background-clip: text;
+    background-clip: text;
+}
 </style>
 
 <template>
     <main>
-        <p>hello caigua</p>
+        <p class="p1">hello caigua</p>
+        <p class="p2">nice to meet you</p>
     </main>
 </template>
 
