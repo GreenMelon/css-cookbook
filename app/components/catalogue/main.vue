28269aee25e76d43541d357a5716d25e74763bb6
caigua <caigua@huanleguang.com>
2020-06-18 10:06:53 +0800

    :art: 重构路由

--- app/components/catalogue/main.vue
+++ app/components/catalogue/main.vue
@@ -24,7 +24,7 @@ export default {
 
     methods: {
         getTableOfContents(name) {
-            const files = require.context('../pages', true, /\.vue$/);
+            const files = require.context('../../pages', true, /\.vue$/);
 
             const paths = files.keys()
                 .map(i => i.replace(/(\.\/|\.vue)/g, ''))
