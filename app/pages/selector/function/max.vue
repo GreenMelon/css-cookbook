95261329e0fedf84a60c895d4216cb5442178bc6
caigua <caigua@huanleguang.com>
2020-08-25 18:29:16 +0800

    :sparkles: max()

---
+++ app/pages/selector/function/max.vue
@@ -0,0 +1,16 @@
+<style lang="css" scoped>
+/** 用于设置一个最小值 */
+div {
+    font-size: max(100px, 100vw - 1200px);
+}
+</style>
+
+<template>
+    <main>
+        <div>caigua</div>
+    </main>
+</template>
+
+<script>
+export default {};
+</script>
