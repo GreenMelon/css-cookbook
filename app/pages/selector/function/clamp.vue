ceea786d2030830bca5cc95adec0b4008f4358c4
caigua <caigua@huanleguang.com>
2020-09-03 14:07:06 +0800

    :sparkles: clamp

---
+++ app/pages/selector/function/clamp.vue
@@ -0,0 +1,21 @@
+<style lang="css" scoped>
+div {
+    width: clamp(200px, 50%, 400px);
+    height: 50px;
+    line-height: 50px;
+    font-size: 24px;
+    text-align: center;
+    color: white;
+    background: pink;
+}
+</style>
+
+<template>
+    <main>
+        <div>clamp</div>
+    </main>
+</template>
+
+<script>
+export default {}
+</script>
