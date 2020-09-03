b77e5f4c5e6a255fb4f785e3525560d4ba26f6c8
caigua <caigua@huanleguang.com>
2020-09-03 14:07:15 +0800

    :sparkles: min

---
+++ app/pages/selector/function/min.vue
@@ -0,0 +1,15 @@
+<style lang="css" scoped>
+div {
+    font-size: min(100px, .5rem);
+}
+</style>
+
+<template>
+    <main>
+        <div>min</div>
+    </main>
+</template>
+
+<script>
+export default {}
+</script>
