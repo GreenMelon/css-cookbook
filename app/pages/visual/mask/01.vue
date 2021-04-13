4b4d8cc1626a49e42471cfa10b1bb26651d49b89
caigua <caigua@huanleguang.com>
2021-04-13 12:08:20 +0800

    :sparkles: mask

---
+++ app/pages/visual/mask/01.vue
@@ -0,0 +1,25 @@
+<style lang="less" scoped>
+img {
+    width: 400px;
+    -webkit-mask: url(~assets/icon-smile.png) center no-repeat;
+    -webkit-mask-size: 170px;
+}
+</style>
+
+<template>
+    <main>
+        <div>
+            <img src="~images/blue-sky.jpg" alt="">
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            times: 100,
+        }
+    },
+}
+</script>
